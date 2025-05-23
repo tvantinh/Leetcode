e1d163cf8677680dffd6cf54eb400bfc08a711a4
tinhbe <98801099+tvantinh@users.noreply.github.com>
2025-05-23 13:59:37 +0700

    Reverse

---
+++ Reverse/Program.cs
@@ -0,0 +1,27 @@
+﻿public class Solution
+{
+    public int Reverse(int x)
+    {
+        int Max = (int)Math.Pow(2, 31) - 1;
+        int Min = (int)Math.Pow(-2, 31);
+
+        int a = 0;
+        int reverse = 0;
+        while (x != 0)
+        {
+            if (reverse > Max / 10 || reverse < Min / 10)
+                return 0;
+            a = (x % 10);
+            reverse = reverse * 10 + a;
+            x = x / 10;
+        }
+        return reverse;
+    }
+    public static void Main()
+    {
+        Solution solution = new Solution();
+        int x = 1534236469;
+        int result = solution.Reverse(x);
+        Console.WriteLine(result);
+    }
+}
---
+++ ZeroArrayTransformation/Program.cs
@@ -0,0 +1,26 @@
+﻿public class Solution
+{
+    public bool IsZeroArray(int[] nums, int[][] queries)
+    {
+        for (int i = 0; i < nums.Length; i++)
+        {
+            int count = 0;//tính để đủ
+            for (int j = 0; j < queries.Length; j++)
+            {
+                if (i >= queries[j][0] && i <= queries[j][1])
+                    count++;
+            }
+            if (nums[i] > count)
+                return false;
+        }
+        return true;
+    }
+    public static void Main()
+    {
+        Solution solution = new Solution();
+        int[] nums = {2};
+        int[][] queries = { new int[] { 0, 0 }, new int[] { 0, 0 } };
+        bool result = solution.IsZeroArray(nums, queries);
+        Console.WriteLine(result); // Output: false
+    }
+}
